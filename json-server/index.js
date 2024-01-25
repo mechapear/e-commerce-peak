b49a4a55a4646ccb1819342341753885386aefcf
mechapear <mechapear@gmail.com>
2024-01-26 00:40:55 +0700

    feat: add get product by id endpoint to api

--- json-server/index.js
+++ json-server/index.js
@@ -7,7 +7,8 @@ app.use(cors());
 
 let jsonData = require("./db.json");
 
-app.get("/api/products",handleGetProducts);
+app.get("/api/products", handleGetProducts);
+app.get("/api/product/:id", handleGetProduct);
 
 app.listen(3000, function () {
   console.log("API is ready at http://localhost:3000");
@@ -15,4 +16,19 @@ app.listen(3000, function () {
 
 function handleGetProducts(req, res) {
   res.send(jsonData.products);
+}
+
+function handleGetProduct(req, res) {
+  // Usign req.params to get a URL params value
+  const id = Number(req.params.id);
+  const selectedProduct = jsonData.products.find((product) => {
+    return product.id === id
+  })
+  
+  // Handle if product not found
+  if(selectedProduct){
+    res.status(200).send(selectedProduct);
+  } else {
+    res.status(404).send(null);
+  }
 }
