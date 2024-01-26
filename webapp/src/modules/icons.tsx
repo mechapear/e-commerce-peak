2bc2254977573469835e901133c1aafa872f41aa
mechapear <mechapear@gmail.com>
2024-01-26 18:00:46 +0700

    fix: invalid svg properties

--- webapp/src/modules/icons.tsx
+++ webapp/src/modules/icons.tsx
@@ -39,8 +39,8 @@ export const MenuIcon = () => {
       className="h-6 w-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
       />
     </svg>
