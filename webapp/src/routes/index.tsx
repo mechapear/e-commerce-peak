b2d34a34a33b67f464750a4832b4de7daa177d02
mechapear <mechapear@gmail.com>
2024-01-26 11:54:33 +0700

    fix: handle the home page is unavailable, redirect users to the products page

--- webapp/src/routes/index.tsx
+++ webapp/src/routes/index.tsx
@@ -1,11 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
-// TODO: now we don't have a home page -> handle this by redirect to products page
-
+// now we don't have a home page
+// handle this by redirect user to the products page /products
 export const Route = createFileRoute('/')({
-  component: HomePage,
+  beforeLoad: () => {
+    throw redirect({
+      to: '/products',
+    })
+  },
 })
-
-function HomePage() {
-  return <h1>This is Home page!</h1>
-}
